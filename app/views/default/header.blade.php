9c7348e1bfe386c32d41cf0eeda53370d95d67cc
Andrew Campbell <andrew.campbell86@yahoo.com>
2014-04-22 00:51:55 -0400

    Fixing site icons.

--- app/views/default/header.blade.php
+++ app/views/default/header.blade.php
@@ -2,6 +2,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	{{ HTML::style("/css/wi3.min.css") }}
+	{{ HTML::style("/css/jquery.mobile.icons.min.css") }}
 	{{ HTML::style("http://code.jquery.com/mobile/1.4.2/jquery.mobile.structure-1.4.2.min.css") }}
-	{{ HTML::script("http://code.jquery.com/jquery-1.9.1.min.js") }}
-	{{ HTML::script("http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js") }}
+	{{ HTML::script("http://code.jquery.com/jquery-1.10.2.min.js") }}
+	{{ HTML::script("http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js") }}
